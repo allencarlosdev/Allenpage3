c521f9aa03063f09079ada2dd461eec4030cf605
CarlosAllen93 <allencarlosdev@gmail.com>
2022-04-30 15:32:31 -0400

    Fixing text color of the Login and Register

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -60,8 +60,8 @@
                 @else
 
                     <div>
-                        <a href="{{ route('login') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log In</a>
-                        <a href="{{ route('register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
+                        <a href="{{ route('login') }}" class="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log In</a>
+                        <a href="{{ route('register') }}" class="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
                     </div>
 
 
