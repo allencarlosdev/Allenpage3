0c44153308ec75377a3da10165a1f9ef7b518500
CarlosAllen93 <allencarlosdev@gmail.com>
2022-05-03 22:37:42 -0400

    Editing the dashboard AdminLTE

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Allenpage3')
+
+@section('content_header')
+    <h1>List of Categories</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+@stop
+
